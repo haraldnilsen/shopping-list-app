37e69272fc6f2bfec75a43e5d6cc4574babf9ad5
haraldnilsen <harald_998@hotmail.com>
2023-08-20 12:59:56 +0200

    new header

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import "react-native-gesture-handler";
 import React from "react";
-import { StyleSheet, SafeAreaView } from "react-native";
+import { StyleSheet, SafeAreaView, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./screens/home";
 import { NativeBaseProvider } from "native-base";
+import { SvgUri } from "react-native-svg";
 
 const Stack = createNativeStackNavigator();
 
@@ -13,7 +14,33 @@ export default function App() {
     <NativeBaseProvider>
       <SafeAreaView style={styles.container}>
         <NavigationContainer>
-          <Stack.Navigator>
+          <Stack.Navigator
+            screenOptions={{
+              header: (props) => (
+                <View style={{ height: 90 }}>
+                  <View
+                    style={{
+                      display: "flex",
+                      flexDirection: "row",
+                      justifyContent: "space-between",
+                    }}
+                  >
+                    <Text>Handlelista</Text>
+                  </View>
+                  <Text
+                    style={{
+                      textAlign: "center",
+                      paddingTop: 10,
+                      fontSize: 30,
+                      color: "#C62015",
+                    }}
+                  >
+                    Handlelista
+                  </Text>
+                </View>
+              ),
+            }}
+          >
             <Stack.Screen name="Handlelista" component={HomeScreen} />
           </Stack.Navigator>
         </NavigationContainer>
