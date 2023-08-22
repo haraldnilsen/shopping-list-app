1b77a66646a83d4d6e03c54893eba66472a7d872
haraldnilsen <harald_998@hotmail.com>
2023-08-22 10:09:01 +0200

    Fixed reanimation error

--- App.tsx
+++ App.tsx
@@ -2,26 +2,27 @@ import "react-native-gesture-handler";
 import React from "react";
 import { StyleSheet, SafeAreaView, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./screens/home";
 import { NativeBaseProvider } from "native-base";
 import Menu from "./assets/menu";
 import Plus from "./assets/plus";
 
-const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
     <NativeBaseProvider>
       <SafeAreaView style={styles.container}>
         <NavigationContainer>
-          <Stack.Navigator>
-            <Stack.Screen
+          <Drawer.Navigator>
+            <Drawer.Screen
               name="Handlelista"
               component={HomeScreen}
               options={{ headerShown: false }}
             />
-          </Stack.Navigator>
+          </Drawer.Navigator>
         </NavigationContainer>
       </SafeAreaView>
     </NativeBaseProvider>
